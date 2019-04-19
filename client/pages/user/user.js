795d33c53c77f99ce84d2f9be4910be1784227c2
宋娜 <nasongyiqiu>
2019-04-19 13:01:27 +0800

    修改，添加关于我们的协议

--- client/pages/user/user.js
+++ client/pages/user/user.js
@@ -59,64 +59,7 @@ Page({
         })
     },
 
-    /*getOrderList: function() {
-        let that = this
-        let args = {
-            userId: app.data.userId,
-            pageSize: that.data.pageSize,
-            pageIndex: that.data.pageIndex + 1
-        }
-        if(args.userId > 0) {
-            netManager.orderSearch(args,
-                function(res) {
-                    console.log('返回值。。。。。',res, res=={})
-                    let list = that.data.orderArray ? that.data.orderArray : []
-                    if (res && res.length > 0) {
-                        that.setData({
-                            orderArray: list.concat(res),
-                            hasMoreData: true,
-                            pageIndex: that.data.pageIndex + 1
-                        })
-                        console.log('订单列表。。。。', that.data)
-                    } else {
-                        that.setData({
-                            orderArray: list,
-                            hasMoreData: false,
-                            pageIndex: that.data.pageIndex + 1
-                        })
-                        console.log('订单列表。。。。', that.data)
-                    }
-                },
-                function(error) {
-                    console.log(error)
-                }
-            )
-        } else {
-            netManager.layer('没有userId！')
-        }
-    },
-
-    getMoreOrder: function() {
-        this.getOrderList()
-    },
-
-    showTicketList: function() {
-        wx.navigateTo({
-            url:"../wallet/wallet"
-        });
-    },
-
-    showOrderList: function () {
-        wx.navigateTo({
-            url:"../orderList/orderList"
-        });
-    },
-
-    showSabisList: function () {
-        wx.navigateTo({
-            url:"../sabis/sabis"
-        });
-    },*/
+  
 
     showAgreement: function () {
         wx.navigateTo({
@@ -142,37 +85,14 @@ Page({
         )
     },
 
-    toFeedback: function() {
-        wx.navigateTo({
-          // url:"../cardApply/cardApply?exCardId=105"
-            url: "../feedback/feedback?orderId=0"
-        });
-    },
+    
     toAboutUs: function() {
         wx.navigateTo({
             url:"../aboutUs/aboutUs"
         });
     },
 
-    /*orderChoose: function(event) {
-        let orderId = event.currentTarget.id
-        let orderArray = this.data.orderArray
-        let orderChoose = null
-        if (orderArray &&  orderId > 0) {
-            for (var i = orderArray.length - 1; i >= 0; i--) {
-                if (orderArray[i].id == orderId) {
-                    orderChoose = orderArray[i]
-                    break
-                }
-            }
-        }
-        console.log(orderId, orderArray, orderChoose)
-        if (orderChoose) {
-            wx.navigateTo({
-                url:"../orderDetail/orderDetail?orderId=" + orderId
-            });
-        }
-    },*/
+    
     phoneCall: function() {
         netManager.makePhoneCall(app.data.kefuPhoneNumber)
     }
