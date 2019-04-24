cd99cab7bd4724fd2c76191d6c5f31cd15cbdbee
unknown <unknown@example.com>
2019-04-24 08:50:18 +0800

    j
    shouquan

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 App({
   globalData:{
     openid:'',
+    userInfo:'',
+    
   },
   getOpenid:function(){
      var that = this;
@@ -11,6 +13,7 @@ App({
        wx.login({
 
          success: res => {
+           console.log(res);
            if(res.code){
              // 发送 res.code 到后台换取 openId, sessionKey, unionId
              wx.request({
@@ -18,7 +21,7 @@ App({
 
                method: 'post',
                success: function (res) {
-                //  console.log('success');
+                //  console.log('success',res);
                  that.globalData.openid = res.data.data.openid;
                 //  wx.setStorage({
                   //  key: '200',
@@ -36,6 +39,35 @@ App({
        })
      })
    },
+  
+  
+  
+  getSetting: function () {
+    var that = this;
+    return new Promise(function (resolve, reject) {
+      wx.getSetting({
+        success: res => {
+          console.log(res)
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                console.log(res.rawData)
+                that.globalData.userInfo = res.rawData
+                resolve(res)
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (that.userInfoReadyCallback) {
+                  that.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+        }
+      })
+    })
+  },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -45,24 +77,28 @@ App({
     
     
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log(this.globalData.userInf)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    return new Promise(function (resolve, reject) {
+      wx.getSetting({
+        success: res => {
+          console.log(res)
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                console.log(res.rawData)
+                that.globalData.userInfo = res.rawData
+                resolve(res)
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (that.userInfoReadyCallback) {
+                  that.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
+      })
     })
   },
   globalData: {
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
   data: {
     id:'',
     motto: 'Hello World',
-    userInfo: {},
+    userInfo: '',
+    name:'',
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     
@@ -18,10 +19,11 @@ Page({
       url: '../logs/logs'
     })
   },
-  
+
   onLoad: function () {
     var _this = this;
     app.getOpenid().then(function(res){
+      console.log(res)
       if(res.code == "200"){
         wx.request({
           url: 'https://sys.songna.top:9090/api/open/wx/user/login',
@@ -36,33 +38,50 @@ Page({
         })
       }
     })
-    if (app.globalData.userInfo) {
-      console.log(userInfo)
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
+    app.getSetting().then(function(res){
+      console.log(res.userInfo,'000000')
+      if (res.userInfo) {
+        _this.setData({
+          name: res.userInfo.nickName,
+          avatar: res.userInfo.avatarUrl,
           hasUserInfo: true
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+      } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
-      })
-    }
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true
+            })
+          }
+        })
+      }
+    })
+    // wx.getUserInfo({
+    //   success: function (res) {
+    //     console.log(res);
+    //     var avatarUrl = 'userInfo.avatarUrl';
+    //     var nickName = 'userInfo.nickName';
+    //     _this.setData({
+    //       [avatarUrl]: res.userInfo.avatarUrl,
+    //       [nickName]: res.userInfo.nickName,
+    //     })
+    //   }
+    // })
+
+    
+    
   },
 
   //这里也是我新加的
